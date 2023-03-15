68d39e1fa87299c4eacb36fcc112cfd7abe74cd6
Fatima Skandani <fatima.skandrani98@gmail.com>
2023-03-15 23:25:19 +0100

    Commencement de la bannière photo

--- src/Pages/home-page/HomePage.js
+++ src/Pages/home-page/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import "./HomePage.css"
+import banner from "../../Images/Banner/Banner.png"
+
 export default function HomePage() {
   return (
-    <div>
-   
+    <div className='homePageContainer'>
+   <img src={banner} className="banner" />
       <div>HomePage</div>
     </div>
   )
