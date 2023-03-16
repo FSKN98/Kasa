f8c6f78bae0050cbe84c9cb49a4a40248f205821
Fatima Skandani <fatima.skandrani98@gmail.com>
2023-03-16 17:17:53 +0100

    mise a jour

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,40 +1,37 @@
-import { BrowserRouter as Router,
+import {
+  BrowserRouter as Router,
   Route,
   Routes,
-  Navigate,} from "react-router-dom";
+  Navigate,
+} from "react-router-dom";
 
 import React from "react";
 import Banner from "../Pages/Banner/BannerHome";
 import ApartmentGrid from "../Pages/apartment-grid/ApartmentGrid";
+import HomePage from "../Pages/home-page/HomePage";
 import ErrorPage from "../Pages/error-page/ErrorPage";
 import ListPage from "../Pages/list/List";
 import Header from "../Commons/Header/Header";
 import Footer from "../Commons/Footer/Footer";
-import APropos from "../Commons/APropos/APropos";
-import FicheLogement from "../Commons/FicheLogement/FicheLogement";
+import About from "../Pages/about/About";
 
 export default function Navigation() {
   return (
     <Router>
-
       <div className="navigation_main_container">
-       
         <main className="navigation_container">
-          <Header/>
-          <Routes>  
-            
-            <Route exact path="/" element={<Banner />} /> 
-                    <Route exact path="/" element={<ApartmentGrid />} />
-            <Route exact path="/a-propos" element={<APropos />} /> 
-             <Route exact path="/fiche-logement" element={<FicheLogement />} /> 
-                      <Route exact path="/list" element={<ListPage/>} />
-                        <Route exact path="/*" element={<ErrorPage />} />
-                        
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<Banner />} />
+            <Route exact path="/" element={<ApartmentGrid />} />
+            <Route exact path="/a-propos" element={<About />} />
+            <Route exact path="/home-page" element={<HomePage />} />
+            <Route exact path="/list" element={<ListPage />} />
+            <Route exact path="/*" element={<ErrorPage />} />
           </Routes>
-          <Footer/>
+          <Footer />
         </main>
       </div>
-      
     </Router>
   );
 }
---
+++ src/Pages/about/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./About.css"
+
+export default function about() {
+  return (
+      <div className='aboutContainer'>
+            <h1 className='banner'></h1>
+    </div>
+  )
+}
