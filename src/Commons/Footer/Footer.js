41f1061f1a99ac088b68ed3f4569a8bc6e94a92c
Fatima Skandani <fatima.skandrani98@gmail.com>
2023-03-20 17:38:26 +0100

    Avancement sur la page HOME, et partie footer terminé

--- src/Commons/Footer/Footer.js
+++ src/Commons/Footer/Footer.js
@@ -1,8 +1,14 @@
-import React from 'react'
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
+import logoFooter from "../../Images/Logos/Logo-Footer.png";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div  className="footerContainer">
+      <div className="footerContain">
+        <img src={logoFooter} className="logoFooter" />
+        <p>© 2020 Kasa. All rights reserved</p>
+      </div>
+    </div>
+  );
 }
--- src/Components/list/List.js
+++ src/Components/list/List.js
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
 import data from "../../json-data/logements (1).json";
-import "./List.css"
+import "./List.css";
 
 const ListPage = () => {
   return (
-    < div className="apartContainer">
-          {data.map((logement) => (
-              <Link to={`/fiche-logement/${logement.id}`}>
+    <div className="apartContainer">
+      {data.map((logement) => (
+        <Link to={`/fiche-logement/${logement.id}`}>
           <div className="apartContent">
-              <div>{logement.title}</div>
-          <img src={logement.pictures[0]} className="imgContainer"/>
-                  </div>
-                  </Link>
+            <img src={logement.pictures[0]} className="imgContainer" />
+            <div className="subtitle">{logement.title}</div>
+          </div>
+        </Link>
       ))}
     </div>
   );
