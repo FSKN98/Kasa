d34dc0ed558e124c0bbcb01759961bf08173c728
Fatima Skandani <fatima.skandrani98@gmail.com>
2023-03-22 16:47:19 +0100

    Page About : Structure, CSS et responsive terminé

--- src/Pages/about/About.js
+++ src/Pages/about/About.js
@@ -1,10 +1,18 @@
-import React from 'react'
-import "./About.css"
+import React from "react";
+import { ApartmentDescription } from "../../Components/apartmentDescription/ApartmentDescription";
+import "./About.css";
 
 export default function about() {
   return (
-      <div className='aboutContainer'>
-            <h1 className='bannerAbout'></h1>
-    </div>
-  )
+    <div className="aboutContainer">
+
+        <h1 className="bannerAbout"></h1>
+<div className="aboutContent">
+      <ApartmentDescription />
+      <ApartmentDescription />
+      <ApartmentDescription />
+      <ApartmentDescription />
+      </div>
+      </div>
+  );
 }
