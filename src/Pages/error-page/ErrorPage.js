e9aa0f2168a13de4a58cd8e2b374250752fc4016
Fatima Skandani <fatima.skandrani98@gmail.com>
2023-03-26 19:13:24 +0200

    Responsive à jour

--- src/Pages/error-page/ErrorPage.js
+++ src/Pages/error-page/ErrorPage.js
@@ -6,10 +6,12 @@ export default function ErrorPage() {
     <div className="errorPageContainer">
       <div className="errorContainer">
         <div className="numberError">404</div>
-        <h2>Oups! La page que vous demandez n'existe pas.</h2>
-        <Link to="/" className="linkError">
-          Retourner sur la page d’accueil
-        </Link>
+        <h2>Oups! La page que <br className="textError"></br> vous demandez n'existe pas.</h2>
+        <div className="linkErrorContain">
+          <Link to="/" className="linkError">
+            Retourner sur la page d’accueil
+          </Link>
+        </div>
       </div>
     </div>
   );
